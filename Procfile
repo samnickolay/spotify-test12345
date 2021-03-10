fdd721adf65885f6bb3e9c76cbd365b041f04330
samnickolay <samnickolay@gmail.com>
2021-03-09 20:35:03 -0800

    test2

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-release: python manage.py migrate --run-syncdb
+release: python manage.py migrate
 
 web: gunicorn backend.wsgi --log-file -
 
--- users/models.py
+++ users/models.py
@@ -1,44 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, is_admin=False, is_staff=False, is_active=True):
-        if not email:
-            raise ValueError("User must have an email")
-        if not password:
-            raise ValueError("User must have a password")
-        # if not full_name:
-        #     raise ValueError("User must have a full name")
-
-        user = self.model(
-            email=self.normalize_email(email)
-        )
-        user.set_password(password)  # change password to hash
-        user.staff = is_staff
-        user.active = is_active
-        user.save(using=self._db)
-        return user
-
-    def create_staffuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-            is_staff=True,
-        )
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-            is_staff=True,
-            is_admin=True,
-        )
-        return user
 
 
 class CustomUser(PermissionsMixin, AbstractBaseUser):
