4b98938f7479a727c86df68a9e3c51d53a067263
samnickolay <samnickolay@gmail.com>
2021-03-10 11:12:16 -0800

    test2

--- public_script.py
+++ public_script.py
@@ -1,7 +1,9 @@
-import requests 
+import requests
 import sys
 import time
- 
+import subprocess
+
+
 s = '{{ sleep 5; printf "\n"; sleep 1; printf "{}"; sleep 1; printf "\t";  printf "{}"; sleep 1; printf "\t"; sleep 1; printf "\n"; sleep 5; printf ":focus search\n"; sleep 1; printf "spotify:album:4PgleR09JVnm3zY1fW3XBA"; sleep 5; printf "\n\n"; sleep 1;  printf "r\n";}} | ncspot'
 
 base_url = 'https://spotify-test12345.herokuapp.com/api/account/'
@@ -10,8 +12,8 @@ key = sys.argv[1]
 
 print(key)
 
-r = requests.get(url = base_url + 'get/', headers = {'Authorization': 'Api-Key ' + key}) 
-data = r.json() 
+r = requests.get(url=(base_url + 'get/'), headers={'Authorization': 'Api-Key ' + key})
+data = r.json()
 
 print(data)
 
@@ -19,14 +21,13 @@ s_final = s.format(data['email'], data['password'])
 
 print(s_final)
 try:
-	rc = call(script, shell=True)
-except _e as Exception:
-	print(_e)
+    rc = subprocess.call(s_final, shell=True)
+except Exception as _e:
+    print(_e)
 
 while True:
-	print('sending update to backend')
-	print(base_url + str(data['pk']) + '/')
-	r = requests.get(url = base_url + str(data['pk']) + '/', headers = {'Authorization': 'Api-Key ' + key}) 
-	print(r)
-	time.sleep(300)
-
+    print('sending update to backend')
+    print(base_url + str(data['pk']) + '/')
+    r = requests.get(url=(base_url + str(data['pk']) + '/'), headers={'Authorization': 'Api-Key ' + key})
+    print(r)
+    time.sleep(300)
