85be6b223030488acd126488ba21531874d1081c
samnickolay <samnickolay@gmail.com>
2021-03-16 12:21:11 -0700

    test

--- public_script.sh
+++ public_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo apt install -y collectd 
+sudo apt-get update  
+sudo apt-get install -y collectd 
 
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb 
 
