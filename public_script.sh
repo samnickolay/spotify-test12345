9be545ff7d2a9e50bcde139f2cd04aa5e9690517
samnickolay <samnickolay@gmail.com>
2021-04-12 18:39:24 -0700

    test

--- public_script.sh
+++ public_script.sh
@@ -122,11 +122,6 @@ sudo snap install spotify --devmode &> /dev/null
 sudo echo '
 #!/bin/bash
 
-sudo mkdir /run/user/1000
-sudo chown -R ubuntu:ubuntu /run/user/1000
-
-
-
 export XDG_RUNTIME_DIR=/run/user/1000
 
 echo "running test!!!" 
@@ -291,11 +286,12 @@ sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 crontab -l > mycron
 #echo new cron into cron file
 echo "
-@reboot sleep 50 && sudo mkdir /run/user/1000 && sudo chown -R ubuntu:ubuntu /run/user/1000 && sleep 10 && export XDG_RUNTIME_DIR=/run/user/1000 && /bin/bash -c '/home/ubuntu/script2.sh $SPOTIFY_EMAIL $SPOTIFY_PASSWORD $PLAYLIST $VPN_NAME $VPN_EMAIL $VPN_PASSWORD' >> /home/ubuntu/ncspot.log 2>&1" >> mycron
+@reboot sleep 20 && sudo mkdir /run/user/1000 && sudo chown -R ubuntu:ubuntu /run/user/1000 && export XDG_RUNTIME_DIR=/run/user/1000 && /bin/bash -c '/home/ubuntu/script2.sh $SPOTIFY_EMAIL $SPOTIFY_PASSWORD $PLAYLIST $VPN_NAME $VPN_EMAIL $VPN_PASSWORD' >> /home/ubuntu/ncspot.log 2>&1" >> mycron
 #install new cron file
 sudo crontab -u ubuntu mycron
 rm mycron
 
+ls -l /run/user/
 sudo mkdir /run/user/1000
 sudo chown -R ubuntu:ubuntu /run/user/1000
 
