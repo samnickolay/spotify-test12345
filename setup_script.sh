dacd00306c5997d546a33cd072df8cde3103f0bf
samnickolay <samnickolay@gmail.com>
2021-04-30 13:25:05 -0700

    test

--- setup_script.sh
+++ setup_script.sh
@@ -15,13 +15,14 @@ env|grep -i runt
 
 chmod 400 test.pem
 
+echo "$1 $2 $3 $4 $5 $6 $7"
 
 # echo "running checker script!"
 # /bin/bash -c './checker_script.sh' &
 
 # sleep 79200 && echo "rebooting after SETUP timeout - 22 hours" && date && sudo reboot &
 
-ssh -tt -o 'StrictHostKeyChecking no' -i ./test.pem ubuntu@localhost "/bin/bash -i -c '/home/ubuntu/script2.sh $1 $2 $3 $4 $5 $6'"
+ssh -tt -o 'StrictHostKeyChecking no' -i ./test.pem ubuntu@localhost "/bin/bash -i -c '/home/ubuntu/script2.sh $1 $2 $3 $4 $5 $6 $7'"
 
 echo "Connection to localhost closed. Restarting!" 
 date
